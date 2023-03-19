42598c38792f5b1ac3cd5d28a47d0d0416058f8f
Jeison Ferrer <jeison120120@hotmail.com>
2023-03-18 19:53:38 -0500

    Primer commit del proyecto

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors')
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req,res)=>{
+    res.send("Bienvenido a ProcenterAPI.");
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Sevidor corriendo en el puerto ${PORT}`);
+})
+
+
+
