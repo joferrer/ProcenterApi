d3478297d6b492fff46a2f1239b223a4f4595684
MarlonPrado <marlonstivenprod@ufps.edu.co>
2023-03-27 23:24:00 -0500

    Conexion con Firestore Database del proyecto, Asbtraccion de un vehiculo de la coleccion Vehiculos

--- index.js
+++ index.js
@@ -4,21 +4,54 @@ const cors = require('cors');
 const {funcionPrueba} = require('./routes/Vehiculos/prueba');
 
 
-
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({extended: true}));
 
-const PORT = process.env.PORT || 3000;
 
 
+const admin = require("firebase-admin");
+const credentials = require("./firebase/key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(credentials)
+});
+const db = admin.firestore();
+
+
+//Rutas del Proyecto
+//Inicial
 app.get('/', (req,res)=>{
     res.send("Bienvenido a ProcenterAPI.");
 })
 
+
+//Vehiculo () => responde con json del raio maquin
+app.get('/vehiculos', async(req, res) => {
+    
+    try {
+        const userRef = db.collection("vehiculos");
+        const response = await userRef.get();
+        let responseArr = [];
+        response.forEach ( doc => {
+            responseArr.push(doc.data());
+        }); 
+        res.send(responseArr);
+        console.log("Houdston lo hemos logrado");
+    }
+    catch (error) {
+        res.send(error);
+    }
+});
+
+
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
-    console.log(`Sevidor corriendo en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
     funcionPrueba();
+    console.log(`Ve a la ruta de vehiculos en localhost:${PORT}/vehiculos`);
 })
 
 
