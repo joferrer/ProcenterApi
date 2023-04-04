9f10653ac40dc5f6ccc172e573ebb7c022d00a16
FractureDVL <joseandrescram1005@gmail.com>
2023-04-04 18:17:00 -0500

    Changes 🗿

--- routes/Usuarios/usuarios.js
+++ routes/Usuarios/usuarios.js
@@ -6,7 +6,10 @@ router.get('/Usuarios',(req,res)=>{
 });
 
 router.post('/catalog',(req,res)=>{
-    res.send('No hay :(')
+    res.send('Se ejecuto post')
+});
+router.get('/catalog',(req,res)=>{
+    res.send('Se ejecuto get')
 });
 
 module.exports = router;
