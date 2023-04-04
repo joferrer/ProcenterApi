b09073320237a93ee1fff70c785db7da73adb19d
FractureDVL <joseandrescram1005@gmail.com>
2023-04-04 18:11:46 -0500

    Se creo ruta para obtener peticiones 🗿

--- routes/Usuarios/usuarios.js
+++ routes/Usuarios/usuarios.js
@@ -3,5 +3,10 @@ const router = express.Router();
 
 router.get('/Usuarios',(req,res)=>{
     res.send('No hay :(')
-})
+});
+
+router.post('/catalog',(req,res)=>{
+    res.send('No hay :(')
+});
+
 module.exports = router;
