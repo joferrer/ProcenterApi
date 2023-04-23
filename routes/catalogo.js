abf8748f2654df833a21b43a7e3f84e52bd45340
FractureDVL <joseandrescram1005@gmail.com>
2023-04-23 00:41:03 -0500

    Front Temporal para visualizar el backend 😂

---
+++ routes/catalogo.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const { actualizarCatalogo } = require('../middlewares/catalogo');
+const { formatoCatalogo } = require('../helpers/formatoCatalogo');
+
+router.get('/catalogo',actualizarCatalogo, (req,res) => {
+    try {
+        let autos = [];
+        req.autos.forEach(e =>{
+            autos.push(formatoCatalogo(e));
+        });
+        res.json(autos);
+
+    } catch (error) {
+        return console.log(error)
+    }
+});
+
+
+module.exports = router;
