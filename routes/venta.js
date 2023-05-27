93aa559b4de0f73911a0e440a9fbbde96a0f7aec
MarlonPrado <marlonstivenprod@ufps.edu.co>
2023-05-27 12:05:26 -0500

    Actualizando rutas de vehiculos

--- routes/venta.js
+++ routes/venta.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { cventa, rventa, rventabyid, uventa, dventa} = require("../middlewares/venta");
+const { cventa, rventa, rventabyid, uventa, dventa, crearVenta} = require("../middlewares/venta");
 const { db, auth } = require("../firebase/providerFirestore");
 const admin = require("firebase-admin");
 
-router.post('/cventa', cventa, (req, res) => {
+router.post('/venta', cventa, (req, res) => {
     res.json();
 });
 
@@ -20,6 +20,10 @@ router.put('/uventa/:id', uventa, (req, res) => {
     res.json();
 });
 router.delete('/dventa/:id', dventa, (req, res) => {
+    res.json();
+});
+
+router.post('/registrar-venta', crearVenta, (req, res) => {
     res.json();
 });
 module.exports = router;
