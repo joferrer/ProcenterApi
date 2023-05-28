a5074f944f45011a6e97c92db923edc39d7771ed
Andres_Camperos <joseandrecram1005@gmail.com>
2023-05-28 15:58:08 -0500

    Added upload image and fix catalog enpoint

--- middlewares/catalogo.js
+++ middlewares/catalogo.js
@@ -1,3 +1,4 @@
+const { object } = require("joi");
 const { db } = require("../firebase/providerFirestore");
 const { traerCatalogo } = require('../helpers/catalogo')
 const axios = require("axios");
@@ -7,10 +8,13 @@ async function consultarCatalogo(req, res, next) {
       const collectionRef = await db.collection('vehiculos');
       const querySnapshot = await collectionRef.where('disponible', '==', true).get();
       const autos = [];
-
-        querySnapshot.forEach((doc) => {
+      if(querySnapshot.empty){
+        req.autos = {estado:false, mensaje: "No hay ningun vehiculo disponible en el catalogo"}
+        next();
+      }
+      querySnapshot.forEach((doc) => {
             autos.push(doc.data());
-        });
+      });
       
       req.autos = autos;
       next();
@@ -46,4 +50,59 @@ async function actualizarPlaca(req, res, next){
     next();
 }
 
-module.exports = { consultarCatalogo, desactivarDisponible }
+async function agregarImagen(imagenes, idVehiculo) {
+    try {
+      const id = idVehiculo;
+      const collectionRef = db.collection('vehiculos').doc(id);
+      let vehiculo = await consultarImagenes(idVehiculo);
+      
+      let imagenIndex = Object.keys(vehiculo).length + 1;
+
+      imagenes.forEach(img => {
+        const clave = `url${imagenIndex}`;
+        vehiculo[clave] = img;
+        imagenIndex++;
+      });
+
+      const updateData = {
+        imagenes: vehiculo
+      };
+  
+      await collectionRef.update(updateData);
+  
+      res.estado = true;
+      res.message = `Las imágenes se han agregado correctamente al vehículo ${idVehiculo}`;
+  
+      res.id = idVehiculo;
+      next();
+    } catch (error) {
+    }
+  }
+  
+  async function consultarImagenes(idVehiculo) {
+    try {
+      const collectionRef = db.collection('vehiculos');
+      const querySnapshot = await collectionRef.where('id', '==', idVehiculo).get();
+     
+      if(querySnapshot.empty){
+        return console.log(`No existe el vehiculo ${idVehiculo}`);
+      }
+     
+      let result = [];
+     
+      querySnapshot.forEach((doc) => {
+        result.push(doc.data());
+      });
+
+      let autos = result[0].imagenes;
+
+      return autos;
+
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
+
+
+module.exports = { consultarCatalogo, desactivarDisponible, actualizarPlaca, agregarImagen }
--- routes/catalogo.js
+++ routes/catalogo.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const { upload, AWS } = require("../awsConfig/bucket");
+const uuid = require("uuid");
 const {
   consultarCatalogo,
   desactivarDisponible,
-  actualizarPlaca
+  actualizarPlaca,
+  agregarImagen,
 } = require("../middlewares/catalogo");
+const keys = require("../awsConfig/aws-credencial.json");
+const fs = require("fs");
+
 const { formatoCatalogo } = require("../helpers/formatoCatalogo");
+const { log } = require("console");
 
 // Desde firestore
 router.get("/catalogo", consultarCatalogo, (req, res) => {
@@ -31,10 +38,46 @@ router.post("/desactivar-vehiculo/:id", desactivarDisponible, (req, res) => {
 });
 
 router.post("actualizarPlaca", actualizarPlaca, (req, res) => {
-    const { idVehiculo, Placa } = req.body;
-    //validar que la placa no exista
-    
-    return res.status(200).send("actualizado")
-})
+  const { idVehiculo, Placa } = req.body;
+  //validar que la placa no exista
+
+  return res.status(200).send("actualizado");
+});
+
+// Ruta para subir la imagen a Amazon S3
+router.post("/subir-imagen", upload.array("image"), async (req, res) => {
+  const s3 = new AWS.S3();
+
+  if (!req.files || req.files.length === 0) {
+    return res
+      .status(400)
+      .json({ estado: false, mensaje: "No se ha proporcionado ninguna imagen" });
+  }
+
+  let imagenes = [];
+
+  try {
+    // Iterar sobre las imágenes subidas
+    for (const file of req.files) {
+      const params = {
+        Bucket: keys.bucketName,
+        Key: `${uuid.v1()}.jpg`,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      };
+
+      // Subir cada imagen a S3
+      const data = await s3.upload(params).promise();
+      imagenes.push(data.Location);
+    }
+    await agregarImagen(imagenes, req.body.id);
+    return res.status(200).send({ estado: true, mensaje:"Imágenes subidas exitosamente"});
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .send({ estado: false, mensaje: "Error al subir las imágenes a S3" });
+  }
+});
 
 module.exports = router;
