dd1f9d38ed1f3d631be85d4107658a7e1e075f53
Andres_Camperos <joseandrecram1005@gmail.com>
2023-06-12 19:21:03 -0500

    fixing azure update images

--- routes/catalogo.js
+++ routes/catalogo.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { db } = require("../firebase/providerFirestore");
-const { upload, AWS } = require("../awsConfig/bucket");
 const uuid = require("uuid");
 const {
   consultarCatalogo,
@@ -11,8 +10,10 @@ const {
   consultarCatalogoRango,
   consultarImagenes,
 } = require("../middlewares/catalogo");
+//AZURE CREDENTIALS
+const azure = require('azure-storage');
+const blobService = azure.createBlobService(process.env.AZURE_STORAGE_ACCOUNT, process.env.AZURE_STORAGE_ACCESS_KEY);
 
-const keys = require("../awsConfig/aws-credentials.json");
 const { traerCatalogo } = require("../helpers/catalogo");
 const { formatoCatalogo } = require("../helpers/formatoCatalogo");
 const { id } = require("../schemas/SchemaVehiculo");
@@ -89,17 +90,15 @@ router.post("/actualizarPlaca", actualizarPlaca, (req, res) => {
   }
 });
 
-// Ruta para subir la imagen a Amazon S3
-router.post("/subir-imagen", upload.array("image"), async (req, res) => {
-  const s3 = new AWS.S3();
+// Ruta para subir la imagen a Amazon S3 
 
-  if (!req.files || req.files.length === 0) {
-    return res
-      .status(400)
-      .json({
-        estado: false,
-        mensaje: "No se ha proporcionado ninguna imagen",
-      });
+router.post('/subir-imagen', async (req, res) => {
+
+  if (!req.body.imagenes || req.body.imagenes.length === 0) {
+    return res.status(400).json({
+      estado: false,
+      mensaje: 'No se ha proporcionado ninguna imagen',
+    });
   }
 
   let imagenes = [];
@@ -107,36 +106,57 @@ router.post("/subir-imagen", upload.array("image"), async (req, res) => {
   try {
     const resultadoConsulta = await consultarImagenes(req.body.id);
     if (!resultadoConsulta) {
-      return res
-        .status(404)
-        .json({
-          estado: false,
-          mensaje: `El vehículo ${req.body.id} no ha sido encontrado`,
+      return res.status(404).json({
+        estado: false,
+        mensaje: `El vehículo ${req.body.id} no ha sido encontrado`,
+      });
+    }
+
+    const containerName = 'procenter2';
+
+    blobService.createContainerIfNotExists(containerName, { publicAccessLevel: 'blob' }, (error) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ estado: false, mensaje: 'Error al crear el contenedor' });
+      }
+
+      for (const base64Image of req.body.imagenes) {
+        const base64Data = base64Image.replace(/^data:image\/jpeg;base64,/, '');
+        const binaryData = Buffer.from(base64Data, 'base64');
+
+        const fileName = `${uuid.v1()}.png`;
+
+        blobService.createBlockBlobFromText(containerName, fileName, binaryData, {
+          contentSettings: {
+            contentType: 'image/png',
+          },
+        }, (error) => {
+          if (error) {
+            console.error(error);
+            return res.status(500).json({ estado: false, mensaje: 'Error al subir la imagen' });
+          }
+
+          const imageUrl = blobService.getUrl(containerName, fileName);
+          imagenes.push(imageUrl);
+          
+          if (imagenes.length === req.body.imagenes.length) {
+            agregarImagen(imagenes, req.body.id)
+              .then(resultadoAgregacion => {
+                return res.status(200).json(resultadoAgregacion);
+              })
+              .catch(error => {
+                console.error(error);
+                return res.status(500).json({ estado: false, mensaje: 'Error al agregar las imágenes' });
+              });
+          }
         });
-    }
-
-    for (const file of req.files) {
-      const params = {
-        Bucket: keys.bucketName,
-        Key: `${uuid.v1()}.png`,
-        Body: file.buffer,
-        ACL: "public-read",
-        ContentType: file.mimetype,
-      };
-
-      const data = await s3.upload(params).promise();
-      imagenes.push(data.Location);
-    }
-
-    const resultadoAgregacion = await agregarImagen(imagenes, req.body.id);
-
-    return res.status(200).send(resultadoAgregacion);
+      }
+    });
   } catch (err) {
     console.error(err);
-    return res
-      .status(500)
-      .send({ estado: false, mensaje: "Error al subir las imágenes a S3" });
+    return res.status(500).json({ estado: false, mensaje: 'Error al subir las imágenes a Azure Blob Storage' });
   }
 });
+
 
 module.exports = router;
